1e684cc53225fed455f49ac6f309ae401f736c91
redhendery <redhendery@gmail.com>
2016-10-11 18:25:53 +1300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
-module.exports = {
-    entry: {
-        helloWorld: './js/helloworld'
-    },
-    output: {
-        filename: 'public/[name].js'
+var cdn = (process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:8080/');
+
+swig.setDefaults({
+    locals: { // Global variables
+        cdn: cdn
     }
+})
+
+function getEntrySources(sources) {
+    if (process.env.NODE_ENV !== 'production') {
+        sources.push('webpack-dev-server/client?http://localhost:8080');
+    }
+
+    return sources;
+}
+
+module.exports = function() {
+    entry: {
+        helloWorld: getEntrySources([
+            './js/helloworld.js'
+        ])
+    }
+  output: {
+    filename: './public/[name].js'
+  }
 };
